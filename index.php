3f1068b1e35413f05a23f94ebab2ba30d7a4223c
David-again <d.ogunbanjo@yahoo.com>
2021-04-12 12:43:49 -0400

    my first commit

---
+++ index.php
@@ -0,0 +1,8 @@
+This is our amazing custom theme.
+<?php 
+    function greet($name, $color){
+        echo "<p> Hi! my name is $name and my favorite color is $color! </p>";
+    }
+
+greet("Danny", "blue");
+?>
